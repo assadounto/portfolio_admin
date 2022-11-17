8100cade03136303196b1ff48d9119319b2f26a2
rich <adukyerer@gmail.com>
2022-11-17 22:21:21 +0000

    Add Migration

--- db/migrate/20221117194457_projects.rb
+++ db/migrate/20221117194457_projects.rb
@@ -1,4 +1,16 @@
 class Projects < ActiveRecord::Migration[7.0]
   def change
+    create_table :projects do |t|
+    t.string :name
+    t.string :place
+    t.string :title
+    t.string :year
+    t.text :description
+    t.string :livesource
+    t.string :sourcecode
+    t.string :cover
+    t.string :thumb
+    t.timestamps
+    end
   end
 end
---
+++ db/migrate/20221117220643_create_project_technologies.rb
@@ -0,0 +1,9 @@
+class CreateProjectTechnologies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :project_technologies do |t|
+      t.references :technology, null: false, foreign_key: true
+      t.references :project, null: false, foreign_key:true
+      t.timestamps
+    end
+  end
+end
