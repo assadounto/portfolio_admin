5fdddf63c8d3437ede643382add7061d81ff7111
rich <adukyerer@gmail.com>
2022-12-02 21:05:30 +0000

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 User.destroy_all
+Technology.destroy_all
+Project.destroy_all
 # Create 1 admin user
 User.create!(
     name: "Richmond",
